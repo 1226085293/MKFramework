8b3524168a5c9d70b208bd5a519bb8b09ef870cf
muzzik <1226085293@qq.com>
2023-04-22 04:21:15 +0800

    还原加载逻辑

--- assets/plugin/project_init.js
+++ assets/plugin/project_init.js
@@ -1,14 +1,35 @@
-self.cc.game.onPreProjectInitDelegate.add(() => {
-	["extend", "decorator", "tool"].reduce((pre, curr) => {
-		return pre.then(() => {
-			return new Promise((resolve, reject) => {
-				self.cc.assetManager.loadBundle(curr, (err, bundle) => {
-					if (err) {
-						return reject(err);
-					}
-					resolve();
+/** 原函数 */
+let old_load_f = self.cc.assetManager.loadBundle;
+/** 初始化任务 */
+let init_task = null;
+
+// 拦截首次加载
+self.cc.assetManager.loadBundle = async function (...args_as) {
+	if (init_task) {
+		await init_task;
+	}
+	// 加载框架
+	else {
+		init_task = ["extend", "config", "framework", "decorator", "tool"]
+			.reduce((pre, name) => {
+				return pre.then(() => {
+					return new Promise((resolve, reject) => {
+						old_load_f.call(self.cc.assetManager, name, (err, bundle) => {
+							if (err) {
+								return reject(err);
+							}
+
+							resolve(bundle);
+						});
+					});
 				});
+			}, Promise.resolve())
+			.then((pre) => {
+				// 还原 loadBundle
+				self.cc.assetManager.loadBundle = old_load_f;
 			});
-		});
-	}, Promise.resolve());
-});
+	}
+
+	// 恢复加载步骤
+	self.cc.assetManager.loadBundle(...args_as);
+};
