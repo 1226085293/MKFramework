e5f482ffa71d7640fb88b2f1a8c8c895cfce81bd
yuanfengxin <1226085293@qq.com>
2023-07-12 17:15:59 +0800

    删除文档站点

--- tool/api-extractor/script/build_doc.ts
+++ tool/api-extractor/script/build_doc.ts
@@ -8,12 +8,10 @@ const jsonc = require("jsonc-parser");
 // 参数：[ (0: 不输出, 1: 输出) ]
 (async () => {
 	console.log(argv);
-	if (argv[2] === "1" || !fs.existsSync("./temp")) {
-		child_process.execSync(`npx ts-node ./script/build_dts.ts`);
-	}
-
+	// 更新声明文件
+	child_process.execSync(`npx ts-node ./script/build_dts.ts`);
 	// 删除输出文件夹
 	fs.removeSync(path.join("./temp", "md"));
 	// 生成 md 文件
-	child_process.execSync(`npx api-documenter markdown -i ./temp -o ../doc-website/docs/api`);
+	child_process.execSync(`npx api-documenter markdown -i ./temp -o ./dist/docs`);
 })();
