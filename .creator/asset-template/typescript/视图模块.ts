b2827f6cc39b226643493b8770024f2a054b5ae1
muzzik <1226085293@qq.com>
2024-06-19 01:11:43 +0800

    更新脚本模板

--- .creator/asset-template/typescript/视图模块.ts
+++ .creator/asset-template/typescript/视图模块.ts
@@ -2,12 +2,14 @@ import { _decorator } from "cc";
 import mk from "mk";
 const { ccclass, property } = _decorator;
 
-@ccclass("module")
-export class module extends mk.view_base {
-	/* --------------- 属性 --------------- */
-	/* ------------------------------- 生命周期 ------------------------------- */
+@ccclass("<%UnderscoreCaseClassName%>")
+export class <%UnderscoreCaseClassName%> extends mk.view_base {
+	// 初始化视图
 	// create(): void {}
+	// 有数据初始化
 	// init(init_?: typeof this.init_data): void {}
+	// 无数据初始化
 	// open(): void {}
+	// 模块关闭
 	// close(): void {}
 }
