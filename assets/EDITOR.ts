8fce6200f3e8915799865e241015f7c598d30ce2
muzzik <1226085293@qq.com>
2024-07-09 19:33:56 +0800

    修改打印

--- assets/EDITOR.ts
+++ assets/EDITOR.ts
@@ -2,6 +2,6 @@ import { EDITOR } from "cc/env";
 
 if (EDITOR) {
 	Promise.all([import("protobufjs")]).catch(() => {
-		console.error("项目未初始化，请在项目根目录控制台执行 npm i 完成后重启项目");
+		console.error("项目未初始化，请在项目根目录控制台执行 npm i 完成后重启项目。", "注：当前项目为框架示例项目，并非框架！");
 	});
 }
