cf36fb7dbd2c5e4a0e50097f34f4a4de379dcd4a
muzzik <1226085293@qq.com>
2024-08-01 23:49:02 +0800

    添加静态视图模块、bundle管理器脚本模板

---
+++ .creator/asset-template/typescript/bundle管理器.ts
@@ -0,0 +1,10 @@
+import { _decorator } from "cc";
+import mk from "mk";
+import cc from "cc";
+const { ccclass, property } = _decorator;
+
+class <%UnderscoreCaseClassName%> extends mk.bundle_.bundle_manage_base {
+	name_s = "这里填写 bundle 名";
+}
+
+export default new <%UnderscoreCaseClassName%>();
