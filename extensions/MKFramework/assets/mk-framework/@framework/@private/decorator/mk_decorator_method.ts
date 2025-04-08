2bae2bc7251c9e3cdc54edf2e3a7e31c471e0d9f
yuanfengxin <1226085293@qq.com>
2025-04-08 10:36:10 +0800

    移动未使用装饰器

--- extensions/MKFramework/assets/mk-framework/@framework/@private/decorator/mk_decorator_method.ts
+++ extensions/MKFramework/assets/mk-framework/@framework/@private/decorator/mk_decorator_method.ts
@@ -6,24 +6,6 @@ class mk_decorator_method extends mk_instance_base {
 	reset_parent_declaration(target_: any, key_s_: string, descriptor_: PropertyDescriptor): void {
 		descriptor_.value = target_.__proto__[key_s_];
 	}
-
-	/** 不重复执行 */
-	not_repeat(target_: any, key_s_: string, descriptor_: PropertyDescriptor): void {
-		/** 运行状态键 */
-		const key = Symbol("decorator_method_not_repeat:" + key_s_);
-		/** 原函数 */
-		const old_value_f = descriptor_.value;
-
-		descriptor_.value = async function (...args_as: any[]) {
-			if (this[key]) {
-				return;
-			}
-
-			this[key] = true;
-			await old_value_f.call(this, ...args_as);
-			this[key] = false;
-		};
-	}
 }
 
 export default mk_decorator_method.instance();
