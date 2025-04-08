f965da96344f78173e04dbaa62abcf25f3e811e4
yuanfengxin <1226085293@qq.com>
2025-04-08 13:46:32 +0800

    修复报错

--- assets/resources/module/language/resources_language.ts
+++ assets/resources/module/language/resources_language.ts
@@ -25,11 +25,11 @@ export class resources_language extends mk.view_base {
 	}
 
 	button_chinese(): void {
-		mk.language_manage.type_s = global_config.language.type.zh_cn;
+		mk.language_manage.type_s = global_config.language.types.zh_cn;
 	}
 
 	button_english(): void {
-		mk.language_manage.type_s = global_config.language.type.en_us;
+		mk.language_manage.type_s = global_config.language.types.en_us;
 	}
 	/* ------------------------------- 功能 ------------------------------- */
 	/* ------------------------------- 网络事件 ------------------------------- */
