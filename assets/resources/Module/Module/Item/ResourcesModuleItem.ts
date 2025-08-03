bb52a15f6f886ce1c355434d679a4bfc51c9b046
muzzik <1226085293@qq.com>
2025-08-04 02:31:56 +0800

    适配模块

--- assets/resources/Module/Module/Item/ResourcesModuleItem.ts
+++ assets/resources/Module/Module/Item/ResourcesModuleItem.ts
@@ -6,10 +6,14 @@ const { ccclass, property } = _decorator;
 
 @ccclass("ResourcesModuleItem")
 export class ResourcesModuleItem extends mk.ViewBase {
+	/* --------------- 属性 --------------- */
+	@property({ displayName: "名字", type: cc.Node })
+	nameNode: cc.Node = null!;
 	/* --------------- public --------------- */
+	initData!: typeof ResourcesModuleItem.prototype.data;
 	data = {
 		/** 描述 */
-		descStr: "",
+		nameStr: "",
 		/** 视图 */
 		view: null as cc.Constructor<mk.ViewBase> | Function | null,
 	};
@@ -17,6 +21,8 @@ export class ResourcesModuleItem extends mk.ViewBase {
 	/* ------------------------------- 生命周期 ------------------------------- */
 	init(init_?: typeof this.initData): void {
 		Object.assign(this.data, this.initData);
+
+		mk.N(this.nameNode).label.string = this.data.nameStr;
 	}
 
 	// open(): void {}
@@ -24,11 +30,11 @@ export class ResourcesModuleItem extends mk.ViewBase {
 	// close(): void {}
 
 	/* ------------------------------- 按钮事件 ------------------------------- */
-	buttonClick(): void {
-		if (this.data.view instanceof Function) {
-			(this.data.view as Function)();
-		} else if (this.data.view) {
-			mk.uiManage.open(this.data.view);
+	clickSelf(): void {
+		if (cc.js.getSuper(this.data.view as any) === mk.ViewBase) {
+			mk.uiManage.open(this.data.view as any);
+		} else {
+			(this.data.view as any)();
 		}
 	}
 }
--- assets/resources/Module/Module/ResourcesModule.ts
+++ assets/resources/Module/Module/ResourcesModule.ts
@@ -1,3 +1,4 @@
+import * as cc from "cc";
 import { _decorator } from "cc";
 import mk from "mk";
 import { ResourcesModuleLayerControl } from "./LayerControl/ResourcesModuleLayerControl";
@@ -6,16 +7,30 @@ import { ResourcesModuleShowAlone } from "./ShowAlone/ResourcesModuleShowAlone";
 import { ResourcesModuleUIStack } from "./UIStack/ResourcesModuleUIStack";
 import { ResourcesModuleWindow } from "./Window/ResourcesModuleWindow";
 import resources_module_mvc from "./MVC/ResourcesModuleMVC";
+import tool from "db://assets/Tool/Tool";
+import { ResourcesModuleItem } from "./Item/ResourcesModuleItem";
 
 const { ccclass, property } = _decorator;
 
 @ccclass("ResourcesModule")
 export class ResourcesModule extends mk.ViewBase {
-	/* --------------- static --------------- */
 	/* --------------- 属性 --------------- */
+	@property({ displayName: "列表", type: cc.Node })
+	listNode: cc.Node = null!;
 	/* --------------- public --------------- */
 	data = {
-		functionList: [
+		functionList: [],
+	};
+
+	/* ------------------------------- 生命周期 ------------------------------- */
+	// init(init_?: typeof this.init_data): void {}
+	open(): void {
+		mk.uiManage.regis(ResourcesModuleLifeCycle, "db://assets/resources/Module/Module/LifeCycle/ResourcesModuleLifeCycle.prefab", this);
+		mk.uiManage.regis(ResourcesModuleShowAlone, "db://assets/resources/Module/Module/ShowAlone/ResourcesModuleShowAlone.prefab", this);
+		mk.uiManage.regis(ResourcesModuleLayerControl, "db://assets/resources/Module/Module/LayerControl/ResourcesModuleLayerControl.prefab", this);
+		mk.uiManage.regis(ResourcesModuleUIStack, "db://assets/resources/Module/Module/UIStack/ResourcesModuleUIStack.prefab", this);
+		mk.uiManage.regis(ResourcesModuleWindow, "db://assets/resources/Module/Module/Window/ResourcesModuleWindow.prefab", this);
+		const dataList: ResourcesModuleItem["initData"][] = [
 			{
 				nameStr: "生命周期",
 				view: ResourcesModuleLifeCycle,
@@ -42,30 +57,15 @@ export class ResourcesModule extends mk.ViewBase {
 					const mvc = new resources_module_mvc();
 				},
 			},
-		],
-	};
+		];
 
-	/* --------------- protected --------------- */
-	/* --------------- private --------------- */
-	/* ------------------------------- 生命周期 ------------------------------- */
-	// init(init_?: typeof this.init_data): void {}
-	open(): void {
-		mk.uiManage.regis(ResourcesModuleLifeCycle, "db://assets/resources/Module/Module/LifeCycle/ResourcesModuleLifeCycle.prefab", this);
-		mk.uiManage.regis(ResourcesModuleShowAlone, "db://assets/resources/Module/Module/ShowAlone/ResourcesModuleShowAlone.prefab", this);
-		mk.uiManage.regis(ResourcesModuleLayerControl, "db://assets/resources/Module/Module/LayerControl/ResourcesModuleLayerControl.prefab", this);
-		mk.uiManage.regis(ResourcesModuleUIStack, "db://assets/resources/Module/Module/UIStack/ResourcesModuleUIStack.prefab", this);
-		mk.uiManage.regis(ResourcesModuleWindow, "db://assets/resources/Module/Module/Window/ResourcesModuleWindow.prefab", this);
+		tool.node.synchronizedChildNodeNumber(this.listNode, dataList.length);
+		dataList.forEach((v, kNum) => {
+			const node = this.listNode.children[kNum];
+
+			node.getComponent(ResourcesModuleItem)!.init(v);
+		});
 	}
 
 	// close(): void {}
-
-	/* ------------------------------- 按钮事件 ------------------------------- */
-	buttonClose(): void {
-		this.close({
-			isDestroy: false,
-		});
-	}
-	/* ------------------------------- 功能 ------------------------------- */
-	/* ------------------------------- 网络事件 ------------------------------- */
-	/* ------------------------------- 自定义事件 ------------------------------- */
 }
