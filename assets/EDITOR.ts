a7140cdaa76b7821fa0a4eb4140a26d2a43a6f5e
muzzik <1226085293@qq.com>
2025-08-04 00:31:18 +0800

    修改提示

--- assets/EDITOR.ts
+++ assets/EDITOR.ts
@@ -2,6 +2,9 @@ import { EDITOR } from "cc/env";
 
 if (EDITOR) {
 	Promise.all([import("protobufjs")]).catch(() => {
-		console.error("项目未初始化，请在项目根目录控制台执行 npm i 完成后重启项目。", "注：当前项目为框架示例项目，并非框架！");
+		console.error(
+			"项目未初始化，请在项目根目录控制台执行 npm i 完成后重启项目。",
+			"注：当前项目为框架功能 Demo 项目，若想为其他项目安装框架请查看 README"
+		);
 	});
 }
