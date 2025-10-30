646ca25a0944826feef2c97e4c436043c2670112
muzzik <45589707+1226085293@users.noreply.github.com>
2025-10-30 16:13:20 +0800

    修复多次加载资源时填写同一跟随释放对象但是没有释放资源 (#142)

--- extensions/MKFramework/assets/MKFramework/Framework/Resources/MKRelease.ts
+++ extensions/MKFramework/assets/MKFramework/Framework/Resources/MKRelease.ts
@@ -12,13 +12,13 @@ import MKFollowNodeRelease from "./MKFollowNodeRelease";
  */
 class MKRelease {
 	/** 节点集合 */
-	private _nodeSet = new Set<Node>();
+	private _nodeList: Node[] = [];
 	/** 资源集合 */
-	private _assetSet = new Set<Asset>();
+	private _assetList: Asset[] = [];
 	/** 对象集合 */
-	private _objectSet = new Set<MKRelease_.TypeReleaseObject>();
+	private _objectList: MKRelease_.TypeReleaseObject[] = [];
 	/** 回调集合 */
-	private _callbackSet = new Set<MKRelease_.TypeReleaseCallBack>();
+	private _callbackList: MKRelease_.TypeReleaseCallBack[] = [];
 	/* ------------------------------- 功能 ------------------------------- */
 	/**
 	 * 释放对象
@@ -95,16 +95,16 @@ class MKRelease {
 		// 添加引用数据
 		if (object_ instanceof Node) {
 			if (object_.isValid) {
-				this._nodeSet.add(object_);
+				this._nodeList.push(object_);
 			}
 		} else if (object_ instanceof Asset) {
 			if (object_.isValid) {
-				this._assetSet.add(object_);
+				this._assetList.push(object_);
 			}
 		} else if (typeof object_ === "function") {
-			this._callbackSet.add(object_);
+			this._callbackList.push(object_);
 		} else {
-			this._objectSet.add(object_);
+			this._objectList.push(object_);
 		}
 
 		return object_;
@@ -114,25 +114,33 @@ class MKRelease {
 	 * 删除释放对象
 	 * @param object_ 删除跟随模块释放的对象或列表
 	 */
-	delete<T extends MKRelease_.TypeReleaseParamType>(object_: T): void {
+	delete<T extends MKRelease_.TypeReleaseParamType>(object_: T): boolean {
 		if (!object_) {
 			mkLog.error("删除释放对象错误", object_);
 
-			return;
+			return false;
 		}
+
+		let valueList: Array<any> = null!;
 
 		// 添加引用数据
 		if (object_ instanceof Node) {
-			this._nodeSet.delete(object_);
+			valueList = this._nodeList;
 		} else if (object_ instanceof Asset) {
-			this._assetSet.delete(object_);
+			valueList = this._assetList;
 		} else if (typeof object_ === "function") {
-			this._callbackSet.delete(object_);
+			valueList = this._callbackList;
 		} else {
-			this._objectSet.delete(object_);
+			valueList = this._objectList;
 		}
 
-		return;
+		let indexNum = valueList.indexOf(object_);
+
+		if (indexNum !== -1) {
+			valueList.splice(indexNum, 1);
+		}
+
+		return indexNum !== -1;
 	}
 
 	/**
@@ -141,50 +149,50 @@ class MKRelease {
 	 */
 	async release(object_?: MKRelease_.TypeReleaseParamType): Promise<void> {
 		if (object_ instanceof Node) {
-			if (this._nodeSet.delete(object_) && object_.isValid) {
+			if (this.delete(object_) && object_.isValid) {
 				object_.removeFromParent();
 				object_.destroy();
 			}
 		} else if (object_ instanceof Asset) {
-			if (this._assetSet.delete(object_) && object_.isValid) {
+			if (this.delete(object_) && object_.isValid) {
 				object_.decRef();
 			}
 		} else if (typeof object_ === "function") {
-			if (this._callbackSet.delete(object_)) {
+			if (this.delete(object_)) {
 				await object_();
 			}
-		} else if (this._objectSet.delete(object_ as any)) {
+		} else if (this.delete(object_ as any)) {
 			await object_!.release();
 		}
 	}
 
 	/** 释放所有对象 */
 	async releaseAll(): Promise<void> {
-		this._assetSet.forEach((v) => {
+		this._assetList.forEach((v) => {
 			if (v.isValid) {
 				v.decRef();
 			}
 		});
 
-		this._nodeSet.forEach((v) => {
+		this._nodeList.forEach((v) => {
 			if (v.isValid) {
 				v.removeFromParent();
 				v.destroy();
 			}
 		});
 
-		for (const v of this._objectSet) {
+		for (const v of this._objectList) {
 			await v.release();
 		}
 
-		for (const vFunc of this._callbackSet) {
+		for (const vFunc of this._callbackList) {
 			await vFunc();
 		}
 
-		this._assetSet.clear();
-		this._nodeSet.clear();
-		this._objectSet.clear();
-		this._callbackSet.clear();
+		this._assetList = [];
+		this._nodeList = [];
+		this._objectList = [];
+		this._callbackList = [];
 	}
 }
 
