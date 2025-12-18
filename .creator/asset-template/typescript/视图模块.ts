d0d0979d8d93e0c1f6680ac6b857796b86f309c0
muzzik <45589707+1226085293@users.noreply.github.com>
2025-12-18 21:26:49 +0800

    修复脚本模板错误 (#196)

--- .creator/asset-template/typescript/视图模块.ts
+++ .creator/asset-template/typescript/视图模块.ts
@@ -7,7 +7,7 @@ export class <%CamelCaseClassName%> extends mk.ViewBase {
 	// 初始化视图
 	// create(): void {}
 	// 有数据初始化
-	// init(init_?: typeof this.init_data): void {}
+	// init(init_?: typeof this.initData): void {}
 	// 无数据初始化
 	// open(): void {}
 	// 模块关闭
